1732732b49f3d00bb3e374bef4b71f7800d7d2f5
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-08-22 21:57:45 +0600

    Connect to post

--- core/post.php
+++ core/post.php
@@ -18,7 +18,26 @@ class Post{
     }
     // getting posts from our database
     public function read(){
-        
+        //create query 
+        $query = 'SELECT
+            c.name as category_name;
+            p.id,
+            p.category_id,
+            p.title,
+            p.body,
+            p.author,
+            p.created_at
+            FROM
+            '.$this->table. ' p
+            LEFT JOIN
+                 categories c ON p.category_id = c.id
+                 ORDERED BY p.created DESC';
+        //prepare statement 
+        $stmt = $this->conn->prepare($query);
+        //execute query 
+        $stmt->execute();
+
+        return $stmt;
     }
 
 }
