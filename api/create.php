ca2ef56a1b76dbd2cd71297dbb7484024316209b
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-08-25 12:15:12 +0600

    Update

--- api/create.php
+++ api/create.php
@@ -2,7 +2,7 @@
 //headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authoraization,X-Requested-With');
 
 //initializing our api
@@ -14,19 +14,20 @@ $post = new Post($db);
 // get raw posted data
 $data = json_decode(file_get_contents("php://input"));
 
-$post->id = $data->id;
 $post->title = $data->title;
 $post->body = $data->body;
 $post->author = $data->author;
 $post->category_id = $data->category_id;
 
 // create post
-if ($post->update()) {
+if ($post->create()) {
     echo json_encode(
-        array('message'=>'Post Updated. ')
+        array('message'=>'Post Created. ')
     );
 }else{
     echo json_encode(
-        array('message'=> 'Post not Updated')
+        array('message'=> 'Post not Created')
     ) ;
 }
+
+
--- api/update.php
+++ api/update.php
@@ -2,7 +2,7 @@
 //headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: PUT');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authoraization,X-Requested-With');
 
 //initializing our api
@@ -14,18 +14,19 @@ $post = new Post($db);
 // get raw posted data
 $data = json_decode(file_get_contents("php://input"));
 
+$post->id = $data->id;
 $post->title = $data->title;
 $post->body = $data->body;
 $post->author = $data->author;
 $post->category_id = $data->category_id;
 
 // create post
-if ($post->create()) {
+if ($post->update()) {
     echo json_encode(
-        array('message'=>'Post Created. ')
+        array('message'=>'Post Updated. ')
     );
 }else{
     echo json_encode(
-        array('message'=> 'Post not Created')
+        array('message'=> 'Post not Updated')
     ) ;
 }
