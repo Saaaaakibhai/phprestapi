666c68ec9a29071d92c8ffa50a69df7e989a71e8
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-08-25 11:50:55 +0600

    Update

--- core/post.php
+++ core/post.php
@@ -71,7 +71,7 @@ class Post{
     }
     public function create(){
         // create query 
-        $query = 'INSERT INTO' . $this->table . 'SET title = :title,body= :body,author =:author,category_id= :category_id';
+        $query = ' INSERT INTO ' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id ';
         // prepare statement 
         $stmt = $this->conn->prepare($query);
         // clean data 
@@ -89,6 +89,36 @@ class Post{
         if ($stmt->execute()) {
             return true;
         }
+        // print error if something goes wrong
+        printf("Error %s. \n",$stmt->error);
+        return false;
+    }
+    
+    public function update(){
+        // Update post query 
+        $query = ' UPDATE ' . $this->table . ' 
+        SET title = :title, body = :body, author = :author, category_id = :category_id 
+        WHERE id = :id ';
+        // prepare statement 
+        $stmt = $this->conn->prepare($query);
+        // clean data 
+        $this-> title       = htmlspecialchars(strip_tags($this->title));
+        $this-> body        = htmlspecialchars(strip_tags($this->body));
+        $this-> author      = htmlspecialchars(strip_tags($this->author));
+        $this-> category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this-> id          = htmlspecialchars(strip_tags($this->id));
+        // binding of parameters 
+        $stmt->bindparam(':title', $this->title);
+        $stmt->bindparam(':body', $this->body);
+        $stmt->bindparam(':author', $this->author);
+        $stmt->bindparam(':category_id', $this->category_id);
+        $stmt->bindparam(':id', $this->id);
+        
+        // execute the query 
+        if ($stmt->execute()) {
+            return true;
+        }
+
         // print error if something goes wrong
         printf("Error %s. \n",$stmt->error);
         return false;
